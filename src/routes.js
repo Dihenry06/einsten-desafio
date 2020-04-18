83bdb85982b8dc3ef60052bb772c9479777d6731
dihenry06 <dihenry06@gmail.com>
2020-04-18 17:12:18 -0300

    new routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,14 @@
 const routes = require('express').Router();
 
 const UserController = require('./controllers/UserController');
+const SessionController = require('./controllers/SessionController');
 
-routes.post('/users',UserController.store);
+routes.post('/sessions', SessionController.create);
+
+routes.post('/users', UserController.store);
+routes.put('/users', UserController.update);
+
+routes.post('/users/recover', UserController.recoverPassword);
+
 
 module.exports = routes;
