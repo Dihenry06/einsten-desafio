20610e69dc490bd38f9ebade151b9b7d9b713476
dihenry06 <dihenry06@gmail.com>
2020-04-19 20:55:02 -0300

    test unit  and config database test environment

--- knexfile.js
+++ knexfile.js
@@ -16,5 +16,20 @@ module.exports = {
       directory: "./src/database/migrations"
     },
     useNullAsDefault: true
+  },
+
+  test: {
+    client: 'mysql',
+    connection: {
+      host: '127.0.0.1',
+      port : 3306,
+      user: 'root',
+      password: '',
+      database: 'desafio_teste'
+    },
+    migrations: {
+      directory: "./src/database/migrations"
+    },
+    useNullAsDefault: true
   }
 };
