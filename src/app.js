ec1fb2f3b50b4891bf90b6ce1f5d440476230dba
dihenry06 <dihenry06@gmail.com>
2020-04-21 18:28:24 -0300

    at app

--- src/app.js
+++ src/app.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const { errors } = require('celebrate');
-const path = require('path');
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine','ejs');
-
 app.use(express.json());
 app.use(cors());
-app.use('/', require('./routesviews'));
 app.use('/api', require('./routes'));
 app.use(errors());
 
